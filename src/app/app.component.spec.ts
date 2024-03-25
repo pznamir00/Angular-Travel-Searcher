71ddd930326ba0a3577ebf852d0b0531cec439c1
pznamir00 <pznamir00@gmail.com>
2024-03-25 01:59:18 +0100

    test(): fixed app test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
@@ -5,6 +6,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      schemas: [NO_ERRORS_SCHEMA],
       imports: [RouterTestingModule],
       declarations: [AppComponent],
     }).compileComponents();
