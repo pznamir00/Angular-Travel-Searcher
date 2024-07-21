33d86af135323a249049fdf8044f949bab12430c
pznamir00 <pznamir00@gmail.com>
2024-07-21 09:46:39 +0100

    ci(): add cd

---
+++ src/environments/environment.stag.ts
@@ -0,0 +1,9 @@
+import { Environment } from 'src/app/types/environment';
+
+export const environment: Environment = {
+  production: true,
+  clientCredentials: {
+    id: 'MtA7nxruf8BwxO2LA0RLsSMQUOuAN4hV',
+    secret: 'vJKp34x8GJbGR5yE',
+  },
+};
